7ed9ef6e4815fa4a512c6c41d873db0636a5c4aa
syd <student@Awen.local>
2016-09-01 11:09:23 +0800

    syd
    项目

---
+++ ShopDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ShopDemo
+//
+//  Created by student on 16/9/1.
+//  Copyright © 2016年 syd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// 这是我增加的代码
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
